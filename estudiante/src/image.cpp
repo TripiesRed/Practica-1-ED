9956a07b58066447d6144554b894e10992dd629d
TripiesRed <gonzalitoo2spam@gmail.com>
2022-10-04 13:25:54 +0200

    Buenas

---
+++ estudiante/src/image.cpp
@@ -0,0 +1,219 @@
+/**
+ * @file image.cpp
+ * @brief Fichero con definiciones para los métodos primitivos de la clase Image
+ *
+ */
+
+#include <cstring>
+#include <cassert>
+#include <iostream>
+
+#include <image.h>
+#include <imageIO.h>
+
+using namespace std;
+
+/********************************
+      FUNCIONES PRIVADAS
+********************************/
+void Image::Allocate(int nrows, int ncols, byte * buffer){
+    rows = nrows;
+    cols = ncols;
+
+    img = new byte * [rows];
+
+    if (buffer != 0)
+        img[0] = buffer;
+    else
+        img[0] = new byte [rows * cols];
+
+    for (int i=1; i < rows; i++)
+        img[i] = img[i-1] + cols;
+}
+
+// Función auxiliar para inicializar imágenes con valores por defecto o a partir de un buffer de datos
+void Image::Initialize (int nrows, int ncols, byte * buffer){
+    if ((nrows == 0) || (ncols == 0)){
+        rows = cols = 0;
+        img = 0;
+    }
+    else Allocate(nrows, ncols, buffer);
+}
+
+// Función auxiliar para copiar objetos Imagen
+
+void Image::Copy(const Image & orig){
+    Initialize(orig.rows,orig.cols);
+    for (int k=0; k<rows*cols;k++)
+        set_pixel(k,orig.get_pixel(k));
+}
+
+// Función auxiliar para destruir objetos Imagen
+bool Image::Empty() const{
+    return (rows == 0) || (cols == 0);
+}
+
+void Image::Destroy(){
+    if (!Empty()){
+        delete [] img[0];
+        delete [] img;
+    }
+}
+
+LoadResult Image::LoadFromPGM(const char * file_path){
+    if (ReadImageKind(file_path) != IMG_PGM)
+        return LoadResult::NOT_PGM;
+
+    byte * buffer = ReadPGMImage(file_path, rows, cols);
+    if (!buffer)
+        return LoadResult::READING_ERROR;
+
+    Initialize(rows, cols, buffer);
+    return LoadResult::SUCCESS;
+}
+
+/********************************
+       FUNCIONES PÚBLICAS
+********************************/
+
+// Constructor por defecto
+
+Image::Image(){
+    Initialize();
+}
+
+// Constructores con parámetros
+Image::Image (int nrows, int ncols, byte value){
+    Initialize(nrows, ncols);
+    for (int k=0; k<rows*cols; k++) set_pixel(k,value);
+}
+
+bool Image::Load (const char * file_path) {
+    Destroy();
+    return LoadFromPGM(file_path) == LoadResult::SUCCESS;
+}
+
+// Constructor de copias
+
+Image::Image (const Image & orig){
+    assert (this != &orig);
+    Copy(orig);
+}
+
+// Destructor
+
+Image::~Image(){
+    Destroy();
+}
+
+// Operador de Asignación
+
+Image & Image::operator= (const Image & orig){
+    if (this != &orig){
+        Destroy();
+        Copy(orig);
+    }
+    return *this;
+}
+
+// Métodos de acceso a los campos de la clase
+
+int Image::get_rows() const {
+    return rows;
+}
+
+int Image::get_cols() const {
+    return cols;
+}
+
+int Image::size() const{
+    return get_rows()*get_cols();
+}
+
+// Métodos básicos de edición de imágenes
+void Image::set_pixel (int i, int j, byte value) {
+    img[i][j] = value;
+}
+byte Image::get_pixel (int i, int j) const {
+    return img[i][j];
+}
+
+// This doesn't work if representation changes
+void Image::set_pixel (int k, byte value) {
+    // TODO this makes assumptions about the internal representation
+    // TODO Can you reuse set_pixel(i,j,value)?
+    img[0][k] = value;
+}
+
+// This doesn't work if representation changes
+byte Image::get_pixel (int k) const {
+    // TODO this makes assumptions about the internal representation
+    // TODO Can you reuse get_pixel(i,j)?
+    return img[0][k];
+}
+
+// Métodos para almacenar y cargar imagenes en disco
+bool Image::Save (const char * file_path) const {
+    // TODO this makes assumptions about the internal representation
+    byte * p = img[0];
+    return WritePGMImage(file_path, p, rows, cols);
+}
+
+void Image::Invert() {
+    for (int i=0; i<size(); i++)
+        set_pixel(i,255-get_pixel(i));
+}
+
+Image Image::Crop(int nrow, int ncol, int height, int width) const {
+
+    if(ncol>= get_cols() || nrow>= get_rows() || height<=0 || width<=0){
+        width = 0;
+        height = 0;
+    }
+
+    else{
+        if(width+ncol> get_cols())
+            width = get_cols()-ncol;
+
+        if(height+nrow> get_rows())
+            height = get_rows()-nrow;
+    }
+
+    Image newimage (height, width);
+    for(int i= 0; i < width; i++){
+
+        for(int j=0; j < height; j++){
+
+            newimage.set_pixel(i, j, get_pixel(i+ncol,j+nrow));
+        }
+    }
+
+    return newimage;
+}
+
+Image Image::Zoom2X() const {
+
+    int newheight = get_rows()*2;
+    int newwidth = get_cols()*2;
+    Image newimage(newwidth, newheight);
+    byte valor_aux=0;
+
+    //Asignación de pixeles
+    for(int i = 0; i < newwidth; i++){
+
+        for(int j = 0; j < newheight; j++){
+
+            if(j%2 == 0 && i%2 == 0)
+                newimage.set_pixel(i, j, get_pixel(i/2, j/2));   //Filas y columnas pares
+
+            else{
+                valor_aux= (get_pixel(i/2,j/2) + get_pixel(i/2+1,j/2)) /2;
+                newimage.set_pixel(i, j, valor_aux); //Interpolación 3x3
+            }
+        }
+
+    }
+
+    return newimage;
+}
+
---
+++ estudiante/src/zoom.cpp
@@ -0,0 +1,70 @@
+//
+// Created by gonzalomp on 4/10/22.
+//
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+#include <image.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+    char *origen, *destino; // nombres de los ficheros
+    Image image;
+    int coordx, coordy, lado; // datos de las para realizar el zoom de una zona concreta
+
+    // Comprobar validez de la llamada
+    if (argc != 6){
+        cerr << "Error: Numero incorrecto de parametros.\n";
+        cerr << "Uso: negativo <FichImagenOriginal> <FichImagenDestino>\n";
+        exit (1);
+    }
+
+    // Obtener argumentos
+    origen  = argv[1];
+    destino = argv[2];
+    coordx = stoi(argv[3]);
+    coordy = stoi(argv[4]);
+    lado = stoi(argv[5]);
+
+
+
+    // Mostramos argumentos
+    cout << endl;
+    cout << "Fichero origen: " << origen << endl;
+    cout << "Fichero resultado: " << destino << endl;
+
+    // Leer la imagen del fichero de entrada
+    if (!image.Load(origen)){
+        cerr << "Error: No pudo leerse la imagen." << endl;
+        cerr << "Terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+
+    // Mostrar los parametros de la Imagen
+    cout << endl;
+    cout << "Dimensiones de " << origen << ":" << endl;
+    cout << "   Imagen   = " << image.get_rows()  << " filas x " << image.get_cols() << " columnas " << endl;
+
+    // Mostramos los parámetros para realizar el zoom
+    cout << endl;
+    cout << "El valor del coordenada x es: " << coordx << endl;
+    cout << "El valor de la coordenada y es: " << coordy << endl;
+    cout << "El valor del lado es: " << lado << endl;
+
+    Image newimage = image.Crop(coordx,coordy,lado, lado);
+
+    // Guardar la imagen resultado en el fichero
+    if (newimage.Save(destino))
+        cout  << "La imagen se guardo en " << destino << endl;
+    else{
+        cerr << "Error: No pudo guardarse la imagen." << endl;
+        cerr << "Terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+
+    return 0;
+}
