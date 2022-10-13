a1876dd8fea271a92895a93913e9d68460cad768
TripiesRed <gonzalitoo2spam@gmail.com>
2022-10-13 18:45:25 +0200

    comentarios

--- cmake-build-debug/doc_doxygen/html/search/all_7.js
+++ cmake-build-debug/doc_doxygen/html/search/all_7.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['título_20de_20la_20página_20principal_19',['Título de la página principal',['../index.html',1,'']]]
+  ['save_18',['Save',['../classImage.html#adde1007dc6359087dae65fa8cae26448',1,'Image']]],
+  ['set_5fpixel_19',['set_pixel',['../classImage.html#a6e316f867fd3b67a75d78ca66bc12195',1,'Image::set_pixel(int i, int j, byte value)'],['../classImage.html#adf54cab55e991b8fac7d8043507588cb',1,'Image::set_pixel(int k, byte value)']]],
+  ['size_20',['size',['../classImage.html#a78068ea10fef9954ea0e91705997652d',1,'Image']]],
+  ['subsample_21',['Subsample',['../classImage.html#aa00e596a67ec6130922c560049457985',1,'Image']]]
 ];
--- cmake-build-debug/doc_doxygen/html/search/searchdata.js
+++ cmake-build-debug/doc_doxygen/html/search/searchdata.js
@@ -1,9 +1,9 @@
 var indexSectionsWithContent =
 {
-  0: "egilorstw~",
+  0: "cegilorstwz~",
   1: "i",
   2: "i",
-  3: "egilorsw~",
+  3: "cegilorswz~",
   4: "i",
   5: "rt"
 };
